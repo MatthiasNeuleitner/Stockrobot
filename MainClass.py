d1492d1e92dbb23994132ed5e4f94a7afea7d602
JosefHas <josef0809@web.de>
2020-06-20 23:40:38 +0200

    adding simple first stock search

---
+++ MainClass.py
@@ -0,0 +1,6 @@
+from StockData import StockData
+
+def main():
+
+if __name__ == "__main__":
+    main()
---
+++ StockData.py
@@ -0,0 +1,16 @@
+from alpha_vantage.timeseries import TimeSeries
+import json
+import requests
+
+class StockData: 
+    key = 'GCVZOA2YSVHS2MYQ'
+    
+    #only input correct date time... or i hate you ex. :"2020-06-19 15:56" wont work with todays date 
+    # symbol like IBM GOOGL MSFT
+    #TODO: get the date for right now from google or other source...
+    def getStockValueByDatePerMinute(self,date,symbol):
+        r = requests.get('https://www.alphavantage.co/query?function=SMA&symbol='+symbol+'&interval=1min&time_period=50&series_type=open&apikey='+self.key)
+        data = json.loads(json.dumps(r.json()))
+        return data['Technical Analysis: SMA'][date]['SMA']
+    
+    
