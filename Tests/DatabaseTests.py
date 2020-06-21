0d3b1dea0d5767216f5f155af32b96a3bd942851
JosefHas <josef0809@web.de>
2020-06-21 15:54:03 +0200

    Adding tests for debug purpose, no unit tests

---
+++ Tests/DatabaseTests.py
@@ -0,0 +1,25 @@
+
+def main():
+        db = DatabaseController("Test")
+        db.insertTable("Test")
+        db.insertNewColumn("Test","niceData INTEGER")
+        db.dropTable("Test")
+
+        sd = StockData()
+        print(sd.getStockValueByDatePerMinute("2020-06-19 15:56","GOOGL"))
+
+
+if __name__ == "__main__":
+    if __package__ is None:
+        import sys
+        from os import path
+        sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+        # import  your stuff here. Python wont allow import in an higher orden than his own directory. So we trick it
+        from StockData import StockData
+        from DatabaseController import DatabaseController
+        from StockData import StockData
+    else:
+        from ..StockData import StockData
+        from ..DatabaseController import DatabaseController
+        from ..StockData import StockData
+    main()
